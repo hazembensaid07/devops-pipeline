477f951253f2724831e13745f656acc57671ca1d
Hazem <hazembensaid195@gmail.com>
2022-12-06 10:18:20 +0100

    fisrt

--- Jenkinsfile
+++ Jenkinsfile
@@ -16,16 +16,10 @@ pipeline {
      
         stage("get code") {
             steps {
-                 script {
-           // The below will clone your repo and will be checked out to master branch by default.
-           git credentialsId: 'github', url: 'https://github.com/hazembensaid07/spring-crud.git'
-           // Do a ls -lart to view all the files are cloned. It will be clonned. This is just for you to be sure about it.
-           sh "ls -lart ./*" 
-           // List all branches in your repo. 
-           sh "git branch -a"
-           // Checkout to a specific branch in your repo.
-           sh "git checkout master"
-          }
+                script {
+                   
+                 gv.cloneCode()
+                }
             }
         }
         stage("build jar") {
--- script.groovy
+++ script.groovy
@@ -1,9 +1,16 @@
 def cloneCode() {
    //cloning code 
-   sh "git credentialsId: 'github' , url: 'https://github.com/hazembensaid07/spring-crud.git' , branch : 'master'"
-   sh "cat pom.xml"
+// The below will clone your repo and will be checked out to master branch by default.
+           git credentialsId: 'github', url: 'https://github.com/hazembensaid07/spring-crud.git'
+           // Do a ls -lart to view all the files are cloned. It will be clonned. This is just for you to be sure about it.
+           sh "ls -lart ./*" 
+           // List all branches in your repo. 
+           sh "git branch -a"
+           // Checkout to a specific branch in your repo.
+           sh "git checkout master"
 } 
 def buildJar() {
     echo 'building the application...'
     sh 'mvn package'
 } 
+return this
