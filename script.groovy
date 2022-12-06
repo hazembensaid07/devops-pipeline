e6322f20a669229f460d747515a98d9d160dfda5
Hazem <hazembensaid195@gmail.com>
2022-12-06 15:12:32 +0100

    git fix

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 def cloneCode() {
    //cloning code 
 // The below will clone your repo and will be checked out to master branch by default.
-           git credentialsId: 'github', url: 'https://github.com/hazembensaid07/spring-crud.git'
+           git credentialsId: 'github', url: 'https://github.com/hazembensaid07/spring-crud-devops.git'
            // Do a ls -lart to view all the files are cloned. It will be clonned. This is just for you to be sure about it.
            sh "ls -lart ./*" 
            // List all branches in your repo. 
